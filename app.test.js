a92623205f07df622ba420d7cd247cc5790711fd
Mimoun Majd <majdmn6@gmail.com>
2019-07-16 14:17:41 +0100

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,13 +1,8 @@
 'use strict'
 const chai = require('chai')
 const expect = chai.expect
-var http = require('http');
-/*describe('Unit Test app.js', function() {
+describe('Unit Test app.js', function() {
   it('should return true', () => {
     expect(true).to.be.true
   })
-})*/
-http.createServer(function (req, res) {
-  res.write('Hello Jenkins!'); 
-  res.end();
-}).listen(3002);
+}
